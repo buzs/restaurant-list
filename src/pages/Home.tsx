6fc40ba1641cac65180c3a243e9ecdba529837ba
Buz <contato@gabrielhenrique.net>
2021-03-16 17:28:38 -0300

    add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import * as S from "./styles"
+
+import IngredientForm from "../components/IngredientForm";
+import IngredientList from "../components/IngredientList";
+
+const Home: React.FC = () => {
+    return (
+        <S.Wrapper>
+            <IngredientForm />
+            <IngredientList />
+        </S.Wrapper>
+    )
+}
+
+export default Home;
---
+++ src/pages/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    margin: 0 auto;
+    max-width: 70rem;
+    background: #36558f;
+    padding: 3rem;
+    border-radius: 1rem;
+`
