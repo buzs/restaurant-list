98e01a00664d7df7496274dec2bc7b6984d7bc73
Buz <contato@gabrielhenrique.net>
2021-03-16 17:27:32 -0300

    add global types and change context

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
 import React from "react";
 import "./styles.css";
 
-import IngredientList from "./IngredientList";
-import AddIngredientForm from "./AddIngredientForm";
+import GlobalStyle from "./styles/global"
+
+import Home from "./pages/Home";
 
 export const IngredientsListContext = React.createContext<
   IngredientsListContextType
 >({
   ingredients: [],
-  addIngredient: (name: string, weight: string) => {}
+  addIngredient: (name: string, metric: string) => {}
 });
 
 type IngredientsListContextType = {
   ingredients: ingredientType[];
-  addIngredient: (name: string, weight: string) => void;
+  addIngredient: (name: string, metric: string) => void;
 };
 
 type ingredientType = {
   name: string;
-  weight: string;
+  metric: string;
 };
 
-export default function App() {
+const App: React.FC = () => {
   const [ingredients, setIngredients] = React.useState<ingredientType[]>([]);
 
-  const addIngredient = (name: string, weight: string) => {
-    const newIngredient = { name, weight };
-    setIngredients((prevIngredients) => [...prevIngredients, { name, weight }]);
+  const addIngredient = (name: string, metric: string) => {
+    const newIngredient = { name, metric };
+    setIngredients((prevIngredients) => [...prevIngredients, { name, metric }]);
   };
 
   return (
     <IngredientsListContext.Provider value={{ ingredients, addIngredient }}>
-      <div className="App">
-        <AddIngredientForm />
-        <IngredientList />
-      </div>
+      <GlobalStyle />
+      <Home />
     </IngredientsListContext.Provider>
   );
 }
+
+export default App;
