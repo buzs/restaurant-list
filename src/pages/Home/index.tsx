a4f1b2052a27ebed52401a8587721062c321eb9d
Buz <contato@gabrielhenrique.net>
2021-03-17 18:17:21 -0300

    add link in home to /shoplist

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,20 @@ import * as S from "./styles"
 
 import IngredientForm from "../../components/IngredientForm";
 import IngredientList from "../../components/IngredientList";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
     return (
-        <S.Wrapper>
-            <IngredientForm />
-            <IngredientList />
-        </S.Wrapper>
+        <>
+            <S.Title>Adicionar Produtos</S.Title>
+            <S.Wrapper>
+                <IngredientForm />
+                <IngredientList />
+                <Link to="/shoplist">
+                    <S.Link>Ir para a Lista de Compras {">"} </S.Link>
+                </Link>
+            </S.Wrapper>
+        </>
     )
 }
 
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -6,4 +6,27 @@ export const Wrapper = styled.div`
     background: rgba(255, 255, 255, 0.32);
     padding: 3rem;
     border-radius: 1rem;
+
+    display: flex;
+    flex-direction: column;
+`
+
+export const Title = styled.h2`
+    font-size: 4rem;
+    text-align: center;
+    margin-bottom: 1.5rem;
+    color: #8cbdd1;
+`
+
+export const Link = styled.a`
+    font-size: 1.7rem;
+    margin-top: 1.8rem;
+    margin-right: 2.5rem;
+    text-align: end;
+    text-decoration: none;
+    color: #8cbdd1;
+
+    :hover {
+        color: #6c96a7
+    }
 `
